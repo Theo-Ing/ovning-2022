432e657bee2ea07bfb74b70935931fdf5a219bae
Theo <theoi@kth.se>
2022-09-28 09:20:01 +0200

    Cleaned up stray files

--- identical.py
+++
@@ -1,7 +0,0 @@
-a = 2022
-
-b = a
-c = 2022
-
-print(a is b)
-print(a is c)
--- subtract.py
+++
@@ -1,8 +0,0 @@
-a = input("Ditt första värde: ")
-b = input("Ditt andra värde: ")
-
-# a och b är strings, vill ha decimaltal
-a = int(a)
-b = int(b)
-
-print(a - b)
